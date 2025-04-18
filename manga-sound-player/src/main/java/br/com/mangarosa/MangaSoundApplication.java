7512c29ed3e96cb1c9605581a27b6eb5ffcf2eea
Gustavo Muniz dos Anjos <contact@gustavoanjos.com>
2025-04-18 13:54:06 -0300

    feat: Implement music player with repository functionality
    Add song repository management for storing and organizing audio files.
    Develop complete player functionality including playlist creation,
    editing and playback with controls (pause, next, previous).
    Update dependencies and team contact information in README.

--- manga-sound-player/src/main/java/br/com/mangarosa/MangaSoundApplication.java
+++ manga-sound-player/src/main/java/br/com/mangarosa/MangaSoundApplication.java
@@ -1,9 +1,7 @@
 package br.com.mangarosa;
 
-public class MangaSoundApplication
-{
-    public static void main( String[] args ) {
-
-        System.out.println( "Welcome to Manga Sound Player" );
+public class MangaSoundApplication {
+    public static void main(String[] args) {
+        System.out.println("Welcome to MangaRosa Sound");
     }
 }
